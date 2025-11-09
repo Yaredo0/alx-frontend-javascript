cb39a47fff6a949b24f43d2b13abb6826108065b
Yaredo0 <yared4sy@gmail.com>
2025-11-09 21:43:32 +0300

    2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,17 +2,21 @@ interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
   location: string;
+  yearsOfExperience?: number;
   [key: string]: any;
 }
 
-const teacher3: Teacher = {
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Director = {
   firstName: 'John',
-  fullTimeEmployee: false,
   lastName: 'Doe',
   location: 'London',
-  contract: false,
+  fullTimeEmployee: true,
+  numberOfReports: 17,
 };
 
-console.log(teacher3);
+console.log(director1);
