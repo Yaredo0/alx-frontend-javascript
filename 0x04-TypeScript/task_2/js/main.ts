ca79b3f37202993fb2ff9a197b7e486d60fd6944
Yaredo0 <yared4sy@gmail.com>
2025-11-09 22:09:15 +0300

    Task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,11 +39,12 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (salary < 500) {
+  if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
   }
   return new Director();
 }
+
 
 function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
@@ -59,8 +60,3 @@ function executeWork(employee: Director | Teacher): string {
 // Test cases
 console.log(executeWork(createEmployee(200)));   // Getting to work
 console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
-
-// Test cases
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
